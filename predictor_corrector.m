84e39b1027c8ac4bbca3b1e6c6e875c16402b8fb
Bannersx <38147810+Bannersx@users.noreply.github.com>
2020-12-08 01:12:08 -0600

    Update predictor_corrector.m

--- predictor_corrector.m
+++ predictor_corrector.m
@@ -31,4 +31,48 @@ function retval = predictor_corrector(f, y0, w, a, b)
 
   Y
   Yc
+  %%--------------Calculando el polinomio de interpolacion----------------------
+  xu = X;
+  yu = Yc;
+
+  %Creando una matriz de tamaño wxw
+  d=zeros(length(Y));
+  %Colocando el vector Y como la primer columna de la matriz
+  d(:,1) = Yc';
+  %Formación de las diferencias divididas.
+  for k=2:length(X)
+    for j=1:length(X)+1-k
+      d(j,k) = (d(j+1,k-1)-d(j,k-1))/(X(j+k-1)-X(j));
+    endfor
+  endfor
+
+
+  %Formando el polinomio:
+  for w=1:length(X)
+    dq = num2str(abs(d(1,w)));
+    if w>1
+      if X(w-1)<0
+        signo1='+';
+      else
+        signo1 = '-';
+      endif
+    endif
+    if d(1,w)<0
+      signo2 ='-';
+    else
+      signo2 = '+';
+    endif
+    if w == 1
+      acum=num2str(d(1,1));
+    elseif w==2
+      polinomioactual = ['(x' signo1 num2str(abs(X(w-1))) ')'];
+      actual =[dq '*' polinomioactual];
+      acum = [acum signo2 actual];
+    else
+      polinomioactual = [polinomioactual '*' '(x' signo1 num2str(abs(X(w-1))) ')'];
+      actual =[dq '*' polinomioactual];
+      acum = [acum signo2 actual];
+     endif
+  endfor
+  acum
 endfunction
